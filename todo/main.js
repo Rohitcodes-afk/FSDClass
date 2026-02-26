44003c2652527a00d96459bbe5eb5ed122897e75
Rohitcodes-afk <rohitbhatt7502@gmail.com>
2026-02-26 19:12:26 +0530

    hi

---
+++ todo/main.js
@@ -0,0 +1,27 @@
+console.log("welcome to javascript");
+ let a = 10;
+ var b = 20;
+const z = 40;
+
+
+//printing some arthmatic operation
+console.log(a+b+z);
+console.log(a+b+z);
+console.log(a+b+z);
+console.log(a+b+z);
+
+//printing increment and decrement preflix
+ 
+let x = 10;
+let y = ++x;
+
+console.log(y);
+
+//printing increment and decrement postflix
+
+let g = 5;
+let h = g++;
+
+console.log(g);
+console.log(h);
+
