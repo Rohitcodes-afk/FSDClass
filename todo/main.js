87018f542ff8bce82cc517e15feb8a2d567fc316
Rohitcodes-afk <rohitbhatt7502@gmail.com>
2026-02-27 18:00:49 +0530

    hi

--- todo/main.js
+++ todo/main.js
@@ -6,20 +6,20 @@ const z = 40;
 
 //printing some arthmatic operation
 console.log(a+b+z);
-console.log(a+b+z);
-console.log(a+b+z);
-console.log(a+b+z);
+console.log(a-b-z);
+console.log(a*b*z);
+console.log(a/b/z);
 
 //printing increment and decrement preflix
  
-let x = 10;
+let x = 50;
 let y = ++x;
 
-console.log(y);
+console.log(y);0
 
 //printing increment and decrement postflix
 
-let g = 5;
+let g = 500;
 let h = g++;
 
 console.log(g);
