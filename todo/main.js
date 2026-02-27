14407c5009a4cd5a9d75f5375b90f0d035776810
Rohitcodes-afk <rohitbhatt7502@gmail.com>
2026-02-27 19:10:31 +0530

    h

--- todo/main.js
+++ todo/main.js
@@ -1,27 +1,16 @@
-console.log("welcome to javascript");
- let a = 10;
- var b = 20;
-const z = 40;
+function sub(a, b) {
+    console.log(a-b);
+    
+    return a - b;
+
+}
+
+var a = Number(prompt("Enter value of a"));
+var b = Number(prompt("Enter value of b"));
+let outputvalue = sub(a, b);
 
 
-//printing some arthmatic operation
-console.log(a+b+z);
-console.log(a-b-z);
-console.log(a*b*z);
-console.log(a/b/z);
+const outputt = document.getElementById("output");
+outputt.innerHTML = outputvalue;
 
-//printing increment and decrement preflix
- 
-let x = 50;
-let y = ++x;
-
-console.log(y);0
-
-//printing increment and decrement postflix
-
-let g = 500;
-let h = g++;
-
-console.log(g);
-console.log(h);
 
